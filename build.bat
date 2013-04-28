f6d5ab4dd7017d1cefa7e2af1dc7a188fdd86ea7
Nate Moseley <677609+FinalDoom@users.noreply.github.com>
2013-04-28 17:38:47 -0400

    [buildscript] Added parameters, checks, easily customizable paths

--- build.bat
+++ build.bat
@@ -1,20 +1,76 @@
 @ECHO OFF
 
-ECHO Building Project
+REM These are path variables, if your system has things in a different place
+REM Please ensure SIGCHECK has no spaces in it. DOS style short paths may be useful.
 
-C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe /property:Configuration=Release;Platform=x86 FishingForm.csproj
+SET NETLOC=C:\Windows\Microsoft.NET\Framework\v3.5
+SET SIGCHECK=D:\PROGRA~2\SigCheck
+SET ZIP7=D:\Program Files\7-Zip
 
-ECHO.
-ECHO Build Complete
-ECHO.
+REM Check parameters, if those happen
 
-pause
+SET HELPPARAMS=-H HELP /H /HELP
+SET DEPLOYPARAMS=/D /DEPLOY -D DEPLOY
+FOR %%A IN (%*) DO (
+	FOR %%H IN (%HELPPARAMS%) DO (
+		IF /I "%%A"=="%%H" (
+			ECHO This script is used to build FishingForm. Options:
+			ECHO %HELPPARAMS% -- Display this message and exit
+			ECHO %DEPLOYPARAMS% -- Skip building and just make deploy files
+			PAUSE
+			EXIT /B
+		)
+	)
+	FOR %%D IN (%DEPLOYPARAMS%) DO (
+		IF /I "%%A"=="%%D" (
+			SET DEPLOY=1
+		)
+	)
+)
+
+EXIT /B
+
+ECHO Checking Required Programs
+
+SET MISSING=
+IF NOT DEFINED DEPLOY (
+	IF NOT EXIST "%NETLOC%\MSBuild.exe" (
+		SET MISSING=1
+		ECHO %NETLOC%\MSBuild.exe not found. Please ensure proper build files exist or run %~f0 deploy
+	)
+)
+IF NOT EXIST "%SIGCHECK%\sigcheck.exe" (
+	SET MISSING=1
+	ECHO %SIGCHECK%\sigcheck.exe not found. Please download from microsoft site--google it--and adjust paths.
+)
+IF NOT EXIST "%ZIP7%\7z.exe" (
+	SET MISSING=1
+	ECHO %ZIP7%\7z.exe not found. Please ensure it or another zip creation program is installed and adjust paths.
+)
+IF DEFINED MISSING (
+	ECHO.
+	ECHO Required files were not found. Exiting.
+	PAUSE
+	EXIT /B
+)
+
+IF NOT DEFINED DEPLOY (
+	ECHO Building Project
+
+	%NETLOC%\MSBuild.exe /property:Configuration=Release;Platform=x86 FishingForm.csproj
+
+	ECHO.
+	ECHO Build Complete
+	ECHO.
+
+	PAUSE
+)
 
 ECHO.
 ECHO Creating Deployable Archives
 ECHO.
 
-FOR /F "tokens=1-3" %%i IN ('C:\PROGRA~2\SigCheck\sigcheck.exe bin\x86\Release\Fishing.exe') DO (IF "%%i %%j"=="File version:" SET filever=%%k)
+FOR /F "tokens=1-3" %%i IN ('%SIGCHECK%\sigcheck.exe bin\x86\Release\Fishing.exe') DO (IF "%%i %%j"=="File version:" SET filever=%%k)
 
 mkdir deploy\source\Properties
 mkdir deploy\source\images
@@ -49,12 +105,14 @@ del FishingForm_mC-FD.zip
 
 cd deploy
 
-"C:\Program Files\7-Zip\7z" a ..\FishingForm_v%filever%_mC-FD_(with_source).zip -mx9 -tzip *
-"C:\Program Files\7-Zip\7z" a ..\FishingForm_mC-FD_(with_source).zip -mx9 -tzip *
-"C:\Program Files\7-Zip\7z" a ..\FishingForm_v%filever%_mC-FD.zip -mx9 -tzip -x!source\ *
-"C:\Program Files\7-Zip\7z" a ..\FishingForm_mC-FD.zip -mx9 -tzip -x!source\ *
+"%ZIP7%\7z" a ..\FishingForm_v%filever%_mC-FD_(with_source).zip -mx9 -tzip *
+"%ZIP7%\7z" a ..\FishingForm_mC-FD_(with_source).zip -mx9 -tzip *
+"%ZIP7%\7z" a ..\FishingForm_v%filever%_mC-FD.zip -mx9 -tzip -x!source\ *
+"%ZIP7%\7z" a ..\FishingForm_mC-FD.zip -mx9 -tzip -x!source\ *
 
 cd ..\
+
+ECHO %filever% > version
 
 ECHO.
 ECHO Deployable Archives Created:
