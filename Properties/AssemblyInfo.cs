84c24efa624b16a856e56b4388e1e86c4311ba2c
Nate Moseley <677609+FinalDoom@users.noreply.github.com>
2014-03-20 05:11:45 +0800

    Added functionality to deliver a custom message on version updates through the DB, FFACE updated.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("FD")]
 [assembly: AssemblyProduct("FishingForm")]
-[assembly: AssemblyCopyright("Copyright © FinalDoom 2013")]
+[assembly: AssemblyCopyright("Copyright © FinalDoom 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.7")]
-[assembly: AssemblyFileVersion("1.7.1.3")]
+[assembly: AssemblyFileVersion("1.7.1.4")]
