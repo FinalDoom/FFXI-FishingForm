74831c7a69f8cff614e87d7ea52eaf7b43218b11
FinalDoom <FinalDoom@localhost>
2014-03-24 00:34:47 +0800

    [test refactor] Moved functions around, new NUnit and such, trying to make test pass (hard without Windower+FFXI)

---
+++ FishingForm.Test/PopulateFFACEResources.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using FFACETools;
+using NUnit.Framework;
+
+namespace Fishing.Test
+{
+    internal class PopulateFFACEResources : TestActionAttribute
+    {
+        public override void BeforeTest(TestDetails testDetails)
+        {
+            base.BeforeTest(testDetails);
+            string dir = Path.Combine(Util.AssemblyDirectory, "resources");
+            Assert.IsTrue(Directory.Exists(dir),
+                "resources directory ({0}) must exist with Windower resources files (or other tests will fail).", dir);
+            FFACE.WindowerPath = Util.AssemblyDirectory;
+       }
+
+        public ActionTargets Targets
+        {
+            get { return ActionTargets.Suite; }
+        }
+    }
+}
