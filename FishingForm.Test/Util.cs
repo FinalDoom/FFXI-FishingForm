16b77c84601d95edf30d92997b8d81b19ab13871
Nate Moseley <677609+FinalDoom@users.noreply.github.com>
2014-03-24 00:34:47 +0800

    [test refactor] Moved functions around, new NUnit and such, trying to make test pass (hard without Windower+FFXI)

---
+++ FishingForm.Test/Util.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace Fishing.Test
+{
+    static class Util
+    {
+        static public string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+        
+        public static MethodInfo GetMethod(string className, string methodName)
+        {
+            if (string.IsNullOrEmpty(className))
+            {
+                Assert.Fail("className cannot be null or empty");
+            }
+            if (string.IsNullOrEmpty(methodName))
+            {
+                Assert.Fail("methodName cannot be null or whitespace");
+            }
+
+            var cls = Type.GetType(className);
+
+            if (cls == null)
+            {
+                Assert.Fail(string.Format("{0} class not found.", className));
+            }
+
+            var method = cls.GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (method == null)
+            {
+                Assert.Fail(string.Format("{0} method not found", methodName));
+            }
+
+            return method;
+        }
+
+        public static object InvokeMethod(MethodInfo method, object thisObj, params object[] parameters)
+        {
+            return method.Invoke(thisObj, parameters);
+        }
+    }
+}
